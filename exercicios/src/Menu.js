3b11613fed2fc40871c315e71d29bbac8be90c38
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-02-12 20:59:00 -0200

    Componente de Classe e seu Estado

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -4,8 +4,12 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './components/Simples'
 import ParOuImpar from './components/ParOuImpar'
 import { Inverter, MegaSena } from './components/Multi'
+import Contador from './components/Contador'
 
 export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={ 6 }/>,
         navigationOptions: { title: 'Mega Sena' }

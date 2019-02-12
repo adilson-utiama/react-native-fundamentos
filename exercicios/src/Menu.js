3b1371cddb09cca71a9f7b71084f12fd3ec8ca7a
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-02-12 20:13:38 -0200

    Utilizando DrawerNavigator de react-navigation

---
+++ exercicios/src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createDrawerNavigator } from 'react-navigation'
+
+import Simples from './components/Simples'
+import ParOuImpar from './components/ParOuImpar'
+import { Inverter, MegaSena } from './components/Multi'
+
+export default createDrawerNavigator({
+    MegaSena: {
+        screen: () => <MegaSena numeros={ 6 }/>,
+        navigationOptions: { title: 'Mega Sena' }
+    },
+    Inverter: {
+        screen: () => <Inverter texto="React Native!"/>
+    },
+    ParOuImpar: {
+        screen: () => <ParOuImpar numero={ 30 } />,
+        navigationOptions: { title: 'Par ou Impar' }
+    },
+    Simples: {
+        screen: () => <Simples texto="Componente Simples"/>
+    }
+})

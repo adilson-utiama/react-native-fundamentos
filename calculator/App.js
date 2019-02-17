6c623787ab60f3bc84284c32b1322364be123b9b
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-02-16 23:50:35 -0200

    [Projeto Calculator] Componente Button

--- calculator/App.js
+++ calculator/App.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import Button, {} from './src/components/Button'
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <View style={ styles.buttons }>
+          <Button label="AC" />
+          <Button label="/" />
+          <Button label="7" />
+          <Button label="8" />
+          <Button label="9" />
+          <Button label="*" />
+          <Button label="4" />
+          <Button label="5" />
+          <Button label="6" />
+          <Button label="-" />
+          <Button label="1" />
+          <Button label="2" />
+          <Button label="3" />
+          <Button label="+" />
+          <Button label="0" />
+          <Button label="." />
+          <Button label="=" />
+        </View>
       </View>
     );
   }
@@ -14,19 +33,10 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
 });

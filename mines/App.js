80511450539fb92eb74059bc1e999d5a2d0b68f5
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-02-23 00:38:56 -0300

    [Campo Minado] Commit inicial

---
+++ mines/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import params from './src/params'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da Grade: 
+            { params.getRowsAmount() } X { params.getColumnsAmount() }
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});

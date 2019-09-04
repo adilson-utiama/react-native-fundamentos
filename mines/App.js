da30dcfecd40f9622fa5eff7a579a301c72ff695
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-09-04 19:49:04 -0300

    Componentes Field, Mine e Flag

--- mines/App.js
+++ mines/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,18 @@ export default class App extends Component {
         <Text style={styles.instructions}>Tamanho da Grade: 
             { params.getRowsAmount() } X { params.getColumnsAmount() }
         </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened />
+
       </View>
     );
   }

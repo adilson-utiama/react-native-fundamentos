fa644373431e5f9ec9bb1197d1449d1ff790a4d1
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-09-04 23:11:35 -0300

    Implementando a logica do jogo

--- mines/src/functions.js
+++ mines/src/functions.js
@@ -16,8 +16,8 @@ const createBoard = (rows, columns) => {
 }
 
 //Espalha as minas aleatoriamente
-const speradMines = (board, minesAmount) => {
-    const rows = board.length,
+const spreadMines = (board, minesAmount) => {
+    const rows = board.length
     const columns = board[0].length
     let minesPlanted = 0
 
@@ -35,8 +35,81 @@ const speradMines = (board, minesAmount) => {
 //Cria  tabuleiros com as minas plantadas
 const createMinedBoard = (rows, columns, minesAmount) => {
     const board = createBoard(rows, columns)
-    speradMines(board, minesAmount)
+    spreadMines(board, minesAmount)
     return board
 }
 
-export { createMinedBoard }
+//Clona o tabuleiro
+const cloneboard = board => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return { ...field }
+        })
+    })
+}
+
+//Verifica as linhas e colunas ao redor do campo
+const getNeighbors = (board, row, column) => {
+    const neighbors = []
+    const rows = [row - 1, roww, row + 1]
+    const columns = [column - 1, column, column + 1]
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const different = r !== row || c !== column
+            const validRow = r >= 0 && board.length
+            const validColumn = c >= 0 && board[0].length
+            if(different && validRow && validColumn) {
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+    return neighbors
+}
+
+//Verifica se os campos vizinhos são seguros
+const safeNeighborhood = (board, row, column) => {
+    const safes = (result, neighbor) => result && !neighbor.mined
+    return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+//Abre o field e verifica os campos vizinhos
+const openField = (board, row, column) => {
+    const field = board[row][column]
+    if (!field.opened) {
+        field.opened = true
+        if (field.mined) {
+            field.exploded  = true
+        } else if (safeNeighborhood(board, row, column)) {
+            getNeighbors(board, row, column)
+                .forEach(n => openField(board, n.row, n.column))
+        } else {
+            const neighbors = getNeighbors(board, row, column)
+            field.nearMines = neighbors.filter(m => n.mined).length
+        }
+    }
+}
+
+const fields = board => [].concat( ...board )
+
+//Verifica se tem campos minados
+const hasExplosion = board => fields(board)
+    .filter(field => field.exploded).length > 0
+
+//Verrifica se ha campos nao minados e nao abertos nao descobertos
+const pendding = field => (field.mined && !field.flagged) || (!field.mined && !field.opened)
+
+//Verifica se jogador encontrou todos os campos nao minados (WIN)
+const wonGame = board => fields(board).filter(pendding).length === 0
+
+//Mostra todos os campos minados (LOSE)
+const showMines = board => fields(board).filter(field => field.mined)
+    .forEach(field => field.opened = true)
+
+export { 
+    createMinedBoard,
+    cloneboard,
+    openField,
+    hasExplosion,
+    wonGame,
+    showMines
+}
